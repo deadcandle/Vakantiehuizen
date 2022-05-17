33e192eee10bb5a129e3b40cc9b5382310a08b80
Misha <mho-sam-sooi@student.scalda.nl>
2022-05-17 13:19:59 +0200

    code is nog meer armoede geworden; meerdere fotos per huis nu werkend

--- content/huizen.php
+++ content/huizen.php
@@ -15,21 +15,27 @@
         <?php } ?>
     </div>
 </div>
-<div class="row">
+<div class="row" id="huizen">
     <?php
         require("config/db_config.php");
         $conn = new mysqli($host, $user, $pass, $db);
-        $pageData = "select h.huis, h.personen, h.omschrijving, h.prijs, a.afbeelding from afbeeldingen a right outer join huizen h on a.huis_id = h.id";
+        $pageData = "select h.id, h.huis, h.personen, h.omschrijving, h.prijs, a.afbeelding from afbeeldingen a right outer join huizen h on a.huis_id = h.id";
         $result = $conn -> query($pageData);
+        $houseIDS = array();
         if ($result -> num_rows > 0) {
             while ($row = $result -> fetch_assoc()) {
-                echo "<div class='col-3'>";
-                echo "<h3>".$row["huis"]."</h3>";
-                echo "<p>".$row["omschrijving"]."</p>";
-                echo "<img src='images/".$row["afbeelding"]."'></img>";
-                echo "<hr>";
-                echo "<b style='float:right'>€ ".$row["prijs"]."</b>";
-                echo "</div>";
+                if (in_array($row["id"], $houseIDS)) {
+                    echo "<script>var miniImg = document.createElement('img'); miniImg.className = 'miniImg'; miniImg.src = 'images/".$row["afbeelding"]."'; document.getElementById('".$row["id"]."').appendChild(miniImg)</script>";
+                } else {
+                    array_push($houseIDS, $row["id"]);
+                    echo "<div id='".$row["id"]."' class='col-3'>";
+                    echo "<h3>".$row["huis"]."</h3>";
+                    echo "<p>".$row["omschrijving"]."</p>";
+                    echo "<img src='images/".$row["afbeelding"]."'></img>";
+                    echo "<hr>";
+                    echo "<b style='float:right'>€ ".$row["prijs"]."</b>";
+                    echo "</div>";
+                }
             }
         }
         ?>
--- databaseMaker.sql
+++ databaseMaker.sql
@@ -38,22 +38,40 @@ insert into teksten (pagina, titel, tekst) values ("huizen", "de titel Huizen", 
 insert into teksten (pagina, titel, tekst) values ("contact", "de titel Contact", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nam eaque, corporis nostrum quo qui similique quidem culpa explicabo maiores laboriosam dolore alias eveniet, facilis consequatur tempore esse distinctio repudiandae.");
 
 -- Voeg de huizen toe
-insert into huizen (huis, personen, omschrijving, prijs) values ("Plaats1", 8, "dit is een huis omschrijving", 95.00);
-insert into huizen (huis, personen, omschrijving, prijs) values ("Plaats2", 12, "dit is een huis omschrijving", 120.00);
-insert into huizen (huis, personen, omschrijving, prijs) values ("Plaats3", 10, "dit is een huis omschrijving", 110.50);
-insert into huizen (huis, personen, omschrijving, prijs) values ("Plaats4", 16, "dit is een huis omschrijving", 135.95);
+insert into huizen (huis, personen, omschrijving, prijs) values ("Plaats1", 8, "Dit is een huis omschrijving", 95.00);
+insert into huizen (huis, personen, omschrijving, prijs) values ("Plaats2", 12, "Dit is een huis omschrijving", 120.00);
+insert into huizen (huis, personen, omschrijving, prijs) values ("Plaats3", 10, "Dit is een huis omschrijving", 110.50);
+insert into huizen (huis, personen, omschrijving, prijs) values ("Plaats4", 16, "Dit is een huis omschrijving (dit huis lijd aan te veel armoede, er zijn dus geen fotos van.", 135.95);
 
--- afbeeldingen (voor huizen)
+-- afbeeldingen voor de huizen (slechts 3 soorten)
 insert into afbeeldingen (huis_id, afbeelding) values (1, "huis1.jpg");
 insert into afbeeldingen (huis_id, afbeelding) values (2, "huis2.jpg");
-insert into afbeeldingen (huis_id, afbeelding) values (3, "huis3.jpg");
+insert into afbeeldingen (huis_id, afbeelding) values (3, "armoedehuis.jpg");
+
+insert into afbeeldingen (huis_id, afbeelding) values (1, "huis1.jpg");
+insert into afbeeldingen (huis_id, afbeelding) values (2, "armoedehuis.jpg");
+insert into afbeeldingen (huis_id, afbeelding) values (3, "armoedehuis.jpg");
+insert into afbeeldingen (huis_id, afbeelding) values (1, "huis1.jpg");
+insert into afbeeldingen (huis_id, afbeelding) values (2, "foto.png");
+insert into afbeeldingen (huis_id, afbeelding) values (3, "huis2.jpg");
+insert into afbeeldingen (huis_id, afbeelding) values (1, "huis1.jpg");
+insert into afbeeldingen (huis_id, afbeelding) values (2, "huis2.jpg");
+insert into afbeeldingen (huis_id, afbeelding) values (3, "huis1.jpg");
+insert into afbeeldingen (huis_id, afbeelding) values (1, "foto.png");
+insert into afbeeldingen (huis_id, afbeelding) values (2, "foto.png");
+insert into afbeeldingen (huis_id, afbeelding) values (3, "huis1.jpg");
+insert into afbeeldingen (huis_id, afbeelding) values (1, "huis1.jpg");
+insert into afbeeldingen (huis_id, afbeelding) values (2, "huis2.jpg");
+insert into afbeeldingen (huis_id, afbeelding) values (3, "armoedehuis.jpg");
+
+
 
 
 -- (TEST) selecteer huisen en de fotos met mySQL JOIN
 select a.afbeelding, h.huis from afbeeldingen a inner join huizen h on a.huis_id = h.id;
 
 -- dit wordt de nieuwe sql om huizen te selecteren want afbeelding is vereist
-select h.huis, h.personen, h.omschrijving, h.prijs, a.afbeelding from afbeeldingen a inner join huizen h on a.huis_id = h.id;
+select h.id, h.huis, h.personen, h.omschrijving, h.prijs, a.afbeelding from afbeeldingen a right outer join huizen h on a.huis_id = h.id  ;
 
 
 
