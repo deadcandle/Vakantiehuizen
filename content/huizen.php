edc16cac40f6f8c91c3f49abcaad49b990d9d748
Misha <mho-sam-sooi@student.scalda.nl>
2022-05-17 11:18:52 +0200

    wqe

---
+++ content/huizen.php
@@ -0,0 +1,31 @@
+
+<br>
+<div class="row">
+    <div class="col-fluid">
+        <?php
+        require("config/db_config.php");
+        $conn = new mysqli($host, $user, $pass, $db);
+        $pageData = "select titel, tekst from teksten where pagina = 'huizen'";
+        if ($result = $conn -> query($pageData)) { ?>
+        <?php
+            $row = $result -> fetch_assoc();
+            echo "<h1>".$row["titel"]."</h1>";
+            echo "<hp>".$row["tekst"]."</hp>";
+        ?>
+        <?php } ?>
+    </div>
+</div>
+<div class="row">
+    <div class="col-3">e</div>
+    <?php
+        require("config/db_config.php");
+        $conn = new mysqli($host, $user, $pass, $db);
+        $pageData = "select huis, personen, omschrijving, prijs 'huizen'";
+        $result = $conn -> query($pageData);
+        if ($result -> num_rows > 0) {
+            while ($row = $result -> fetch_assoc()) {
+                echo "<hp>".$row["tekst"]."</hp>";
+            }
+        }
+        ?>
+</div>
