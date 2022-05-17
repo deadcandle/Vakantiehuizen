201ba7e446b535d2b8c897e55a8c0be05b4bfae6
Misha <mho-sam-sooi@student.scalda.nl>
2022-05-17 13:46:51 +0200

    Afbeeldingen zijn klikbaar

--- content/huizen.php
+++ content/huizen.php
@@ -25,7 +25,7 @@
         if ($result -> num_rows > 0) {
             while ($row = $result -> fetch_assoc()) {
                 if (in_array($row["id"], $houseIDS)) {
-                    echo "<script>var miniImg = document.createElement('img'); miniImg.className = 'miniImg'; miniImg.src = 'images/".$row["afbeelding"]."'; document.getElementById('".$row["id"]."').appendChild(miniImg)</script>";
+                    echo "<script>var miniImg = document.createElement('img'); miniImg.className = 'miniImg'; miniImg.src = 'images/".$row["afbeelding"]."'; document.getElementById('".$row["id"]."').appendChild(miniImg); miniImg.onclick = function(){document.getElementById('".$row["id"]."').querySelector('img').src='images/".$row["afbeelding"]."'};</script>";
                 } else {
                     array_push($houseIDS, $row["id"]);
                     echo "<div id='".$row["id"]."' class='col-3'>";
@@ -35,7 +35,7 @@
                     echo "<hr>";
                     echo "<b style='float:right'>€ ".$row["prijs"]."</b>";
                     echo "</div>";
-                }
+                    echo "<script>var miniImg = document.createElement('img'); miniImg.className = 'miniImg'; miniImg.src = 'images/".$row["afbeelding"]."'; document.getElementById('".$row["id"]."').appendChild(miniImg); miniImg.onclick = function(){document.getElementById('".$row["id"]."').querySelector('img').src='images/".$row["afbeelding"]."'};</script>";                }
             }
         }
         ?>
