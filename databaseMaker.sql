68540b8ae936f76ac1f33b1f55c10c2a27d27a56
Misha <59292383+Mish6666@users.noreply.github.com>
2022-05-24 11:16:03 +0200

    Update databaseMaker.sql

--- databaseMaker.sql
+++ databaseMaker.sql
@@ -65,11 +65,6 @@ insert into afbeeldingen (huis_id, afbeelding) values (2, "huis2.jpg");
 insert into afbeeldingen (huis_id, afbeelding) values (3, "armoedehuis.jpg");
 
 
-
-
--- (TEST) selecteer huisen en de fotos met mySQL JOIN
-select a.afbeelding, h.huis from afbeeldingen a inner join huizen h on a.huis_id = h.id;
-
 -- dit wordt de nieuwe sql om huizen te selecteren want afbeelding is vereist
 select h.id, h.huis, h.personen, h.omschrijving, h.prijs, a.afbeelding from afbeeldingen a right outer join huizen h on a.huis_id = h.id  ;
 
